bff18ce54edfd2dafe48d9c3c8c4bb17b0029205
Crystal Taylor <kittytaylor@gmail.com>
2021-06-16 23:37:01 -0700

    editing
    Editing code so it will function as a blog.

---
+++ Blog/models/Posts.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
--- Blog/models/index.js
+++ Blog/models/index.js
@@ -1,9 +1,14 @@
 const User = require('./User');
 const Gallery = require('./Gallery');
 const Painting = require('./Painting');
+const Post = require('./Posts');
 
 Gallery.hasMany(Painting, {
   foreignKey: 'gallery_id',
+});
+
+Post.hasMany(Post, {
+  foreignKey: 'id',
 });
 
 Painting.belongsTo(Gallery, {
